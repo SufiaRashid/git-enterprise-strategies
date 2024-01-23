1175e5ba9ab2b95f79c5b0f8c5f7bf07d6cb3495
SufiaRashid <sufiakulthum@email.com>
2024-01-23 15:57:57 -0500

    adding new logo and teammates changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import gatorLogo from "./gator.gif";
+import logo from "./gator.gif";
 import "./App.css";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World! This is our team project.</p>
-        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <p> My name is Sufia Rashid and this is my project </p>
       </header>
